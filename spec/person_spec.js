3da5c090a0e2088b3739fa5d60e2e22416eada35
xolani <xolani.mgube@umuzi.org>
2020-01-16 11:02:13 +0200

    edited the reasons on the spec file

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -3,19 +3,19 @@ let person = require("../src/person.js")
 let xolani = new person("Xolani Mgube", 22, "Male", ["Ping Pong", "Reading"]);
 
 describe("Person Class", function(){
-    it("Sentence", function() {
+    it("Should contain the sentence", function() {
         expect(xolani.hello()).toBe("Hello, my name is Xolani Mgube and I am 22 years old.  My interests are Ping Pong and Reading");
     })
 
-    it("Name", function(){
+    it("Should contain the Name person", function(){
         expect(xolani.name).toBe("Xolani Mgube"); 
     })
 
-    it("Age", function(){
+    it("Should contain the age of new person", function(){
         expect(xolani.age).toBe(22); 
     })
 
-    it("First index of interensts", function(){
+    it("contains the second index of interensts", function(){
         expect(xolani.interests[1]).toBe("Reading")
     })
 })
